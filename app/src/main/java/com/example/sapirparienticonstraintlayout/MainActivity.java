41cd6cd28767291c5da21e6fb834ab10f5f52b87
sapir-parienti <sapir.pa@sisma.org.il>
2024-01-27 16:12:20 +0200

    3

--- app/src/main/java/com/example/sapirparienticonstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sapirparienticonstraintlayout/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.sapirparienticonstraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    Button btn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button);
+
+    }
+
+    public void go(View view) {
+        String s="I've been clicked";
+        btn.setText(s);
+
     }
 }
